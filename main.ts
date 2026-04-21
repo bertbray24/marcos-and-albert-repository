bf40a2b57931e049902a2edf90f8d63a07856e27
bertbray24 <albertbray@enoriveracademy.org>
2026-04-21 11:23:15 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,27 +4,11 @@ namespace SpriteKind {
 /**
  * This Is our unit 5 project. Functions with parameters and return values are used in Launch_Shot, CalculatePoints, Award_Points, and Distance, each taking inputs and returning or producing results based on those values. Conditional statements and boolean operators appear in the blocking mechanic and shooting state system using if and else if chains combined with and and or logic. User input is handled through the Keybinds extension which remaps keyboard controls so both players can play simultaneously on the same keyboard. The game update loop iterates every frame handling ball physics, dribble animation, and win checking. Arrays store dribble offsets, player names, and game messages that are accessed by index throughout the game. Two players compete with independent scores tracked and displayed through the Info extension. A tile map loads the court and defines the play area boundaries. The Info extension serves as the existing extension for live score display on screen, while the custom Keybinds extension by Brohann3214 is the user-added extension that solves MakeCode's default single-player input limitation and enables true simultaneous two-player keyboard gameplay.
  */
-// This two-player basketball game It our Unit 5 project. Functions with parameters and return values are used in Launch_Shot, CalculatePoints, Award_Points, and Distance, each taking inputs and returning or producing results based on those values. Conditional statements and boolean operators appear in the blocking mechanic and shooting state system using if and else if chains combined with and and or logic. User input is handled through the Keybinds extension which remaps keyboard controls so both players can play simultaneously on the same keyboard. The game update loop iterates every frame handling ball physics, dribble animation, and win checking. Arrays store dribble offsets, player names, and game messages that are accessed by index throughout the game. Two players compete with independent scores tracked and displayed through the Info extension. A tile map loads the court and defines the out of bounds. The Info extension serves as the existing extension for live score display on screen, while the custom Keybinds extension by Brohann3214 is the user-added extension that solves MakeCodes default single-player input limitation and enables true simultaneous two-player keyboard gameplay.
-function Jump_Ball () {
-    jump_ball_active = true
-    ball_following = true
-    Ball.setPosition(80, 40)
-    Ball.setVelocity(0, 0)
-    pause(300)
-    ball_following = false
-    Ball.setVelocity(0, 50)
-    pause(800)
-    jump_ball_active = false
-}
 function Launch_Shot (PlayerNum: number) {
     if (Marker_position >= Green_zone_min && Marker_position <= Green_zone_max) {
         if (PlayerNum == 1) {
             Ball.setVelocity(65, -110)
             Ball.follow(Hoop_Right, 100)
-            ball_following = true
-        } else {
-            Ball.setVelocity(-65, -110)
-            Ball.follow(Hoop_Left, 100)
             ball_following = true
         }
     } else {
@@ -38,23 +22,9 @@ function Launch_Shot (PlayerNum: number) {
     Shooting = false
 }
 function Award_Points (playernum: number, points: number) {
-    if (playernum == 1) {
-        Player1score = Player1score + points
-        info.player1.setScore(Player1score)
-    } else {
-        Player2Score = Player2Score + points
-        info.player2.setScore(Player2Score)
-    }
+    Player1score = Player1score + points
+    info.player1.setScore(Player1score)
     music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
-}
-function Check_Winner () {
-    if (Player1score >= 11) {
-        return 1
-    } else if (Player2Score >= 11) {
-        return 2
-    } else {
-        return 0
-    }
 }
 function Distance (spriteA: Sprite, SpriteB: Sprite) {
     dx = spriteA.x - SpriteB.x
@@ -73,27 +43,11 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.UI, function (sprite, otherS
             player1.sayText("Scores!!!", 1500, false)
         }
         Reset_Ball()
-        Jump_Ball()
-    }
-    if (otherSprite == Hoop_Left && Ball_holder == 0) {
-        PTS = CalculatePoints(2)
-        Award_Points(2, PTS)
-        Ball.setVelocity(0, 80)
-        pause(300)
-        if (PTS == 2) {
-            player2.sayText("3-POINTER!!", 1500, false)
-        } else {
-            player2.sayText("Scores!!!", 1500, false)
-        }
-        Reset_Ball()
-        Jump_Ball()
     }
 })
 function CalculatePoints (PlayerNum: number) {
     if (PlayerNum == 1) {
         distance = Math.abs(shot_x - 152)
-    } else if (PlayerNum == 2) {
-        distance = Math.abs(shot_x - 8)
     }
     if (distance > 55) {
         return 2
@@ -105,8 +59,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, ot
     if (Ball_holder == 0 && (Math.abs(Ball.vx) < 10 && (Math.abs(Ball.vy) < 10 && ball_following == false))) {
         if (sprite == player1) {
             Ball_holder = 1
-        } else if (sprite == player2) {
-            Ball_holder = 2
         }
     }
 })
@@ -219,26 +171,6 @@ function Green_Bar2 () {
         `, SpriteKind.UI)
     Hoop_Right.setPosition(143, 74)
     Hoop_Right.setFlag(SpriteFlag.Invisible, true)
-    Hoop_Left = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 1 . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.UI)
-    Hoop_Left.setPosition(17, 74)
-    Hoop_Left.setFlag(SpriteFlag.Invisible, true)
 }
 controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
     if (Ball_holder == 1 && Shooting == false) {
@@ -359,7 +291,6 @@ function Set_up () {
     Player_Names = [" Player 1", " Player 2"]
     Hoop_x = [8, 152]
     info.player1.setScore(0)
-    info.player2.setScore(0)
     player1.ay = 250
     tiles.setCurrentTilemap(tilemap`level1`)
     player1.setStayInScreen(true)
@@ -378,10 +309,8 @@ function Reset_Ball () {
     player1.setPosition(55, 90)
     player1.setVelocity(0, 0)
     Player1jump = false
-    Player2jump = false
     blocked = false
 }
-let Player2jump = false
 let Hoop_x: number[] = []
 let Player_Names: string[] = []
 let Game_messages: string[] = []
@@ -391,29 +320,26 @@ let Dribble_step = 0
 let Winner = 0
 let Bounce_Offset = 0
 let blocked = false
+let jump_ball_active = false
 let Player1jump = false
 let Marker_Direction = 0
 let Marker: Sprite = null
 let Green_Bar: Sprite = null
 let shot_x = 0
 let distance = 0
-let player2: Sprite = null
 let player1: Sprite = null
 let PTS = 0
 let dy = 0
 let dx = 0
-let Player2Score = 0
 let Player1score = 0
 let Shooting = false
 let Ball_holder = 0
-let Hoop_Left: Sprite = null
+let ball_following = false
 let Hoop_Right: Sprite = null
+let Ball: Sprite = null
 let Green_zone_max = 0
 let Green_zone_min = 0
 let Marker_position = 0
-let Ball: Sprite = null
-let ball_following = false
-let jump_ball_active = false
 scene.setBackgroundImage(img`
     cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
     cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
@@ -545,12 +471,10 @@ Keybinds.CustomKey.RIGHT,
 Keybinds.CustomKey.M,
 Keybinds.CustomKey.P
 )
-game.splash("BASKETBALL - Same keyboard, 2 players! First to 11 wins!")
-game.splash("PLAYER 1 (RED): Arrow keys to move | M = Pickup/Shoot | P = Jump/Block")
-game.splash("PLAYER 2 (BLUE): W A S D to move | Q = Pickup/Shoot | E = Jump/Block")
+game.splash("BASKETBALL - Get as many points as you can in 30 seconds")
+game.splash("Arrow keys to move | M = Pickup/Shoot | P = Jump/Block")
 game.splash("Stop the marker in the GREEN zone to score! Miss = random shot")
 Set_up()
-Jump_Ball()
 game.onUpdate(function () {
     if (Shooting == true) {
         Marker_position = Marker_position + Marker_Direction * 4
@@ -564,7 +488,7 @@ game.onUpdate(function () {
         }
         Marker.x = 57 + Marker_position * 45 / 100
     }
-    if (Ball_holder == 1 || Ball_holder == 2) {
+    if (Ball_holder == 1) {
         dribble_Timer = dribble_Timer + 1
         if (dribble_Timer >= 4) {
             dribble_Timer = 0
@@ -573,8 +497,6 @@ game.onUpdate(function () {
         Bounce_Offset = Dribble[Dribble_step]
         if (Ball_holder == 1) {
             Ball.setPosition(player1.x + 8, player1.y + Bounce_Offset)
-        } else {
-            Ball.setPosition(player2.x - 8, player2.y + Bounce_Offset)
         }
         Ball.setVelocity(0, 0)
     }
@@ -599,11 +521,6 @@ game.onUpdate(function () {
         player1.vy = 0
         Player1jump = false
     }
-    if (Player2jump == true && (player2.y >= 90 && player2.vy >= 0)) {
-        player2.y = 90
-        player2.vy = 0
-        Player2jump = false
-    }
     if (Player1jump == true && (blocked == false && (Distance(player1, Ball) < 22 && (Ball_holder == 0 || ball_following == true))) && (Math.abs(Ball.vx) > 20 || Math.abs(Ball.vy) > 20) && jump_ball_active == false) {
         Ball.unfollow()
         ball_following = false
@@ -611,15 +528,8 @@ game.onUpdate(function () {
         Ball.setVelocity(randint(-70, 70), randint(-90, -40))
         player1.sayText("BLOCKED!", 1500, false)
     }
-    if (Player2jump == true && (blocked == false && (Distance(player2, Ball) < 22 && (Ball_holder == 0 || ball_following == true))) && (Math.abs(Ball.vx) > 20 || Math.abs(Ball.vy) > 20) && jump_ball_active == false) {
-        Ball.unfollow()
-        ball_following = false
-        blocked = true
-        Ball.setVelocity(randint(-70, 70), randint(-90, -40))
-        player2.sayText("BLOCKED!", 1500, false)
-    }
     if (Winner == 0) {
-        Winner = Check_Winner()
+        Winner = 0
     }
     if (Winner != 0) {
         game.splash("" + Player_Names[Winner - 1] + " WINS!!")
